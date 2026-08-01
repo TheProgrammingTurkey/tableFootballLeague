2cfab4b20fa7644171bab1d31b25a23b60b2d633
Jack Turkey <96401164+TheProgrammingTurkey@users.noreply.github.com>
2026-08-01 16:52:46 -0400

    Whoops

--- script.js
+++ script.js
@@ -115,13 +115,16 @@ const PERFECT_SHOT = 3;
 
 // Average amount every AI underhits.
 // More negative = more conservative.
+const BASE_UNDERHIT = -.75;
 
 // How much better a 99 aims than a 70.
 // Smaller values = skill matters less.
+const AIM_IMPROVEMENT = .42;
 
 // Error spread (consistency).
 // Higher = more random.
 const WORST_STD_DEV = 1;   // Skill 70
+const BEST_STD_DEV = .5;    // Skill 99
 
 // Reflection strength when going outside 1-5.
 // 0 = hard clamp
