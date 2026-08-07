d7be469f1f5db14233144c64051e9f3440b96205
Jack Turkey <96401164+TheProgrammingTurkey@users.noreply.github.com>
2026-08-07 14:28:08 -0400

    Whoops

--- script.js
+++ script.js
@@ -135,9 +135,7 @@ const REFLECTION = 0.30;
 //If the user picked Quick Play, figure out what team is the home team and what team is the away team
 if(gameType == "quickPlay"){
     let teams = eastStandingsData.concat(westStandingsData);
-    console.log(teams);
     game.homeTeam = teams[Math.floor(Math.random()*16)];
-    console.log(game.homeTeam)
     //Make sure its not a team playing against themselves
     teams.every(team => {
         if(game.homeTeam[0] == team[0]){
